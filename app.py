71db4f371dca8d8df59f86c9aa862ad084f3e4ec
kkittif <kovacs.kitt.f@gmail.com>
2023-01-19 17:35:15 +0100

    Files for Greek god classifier

---
+++ app.py
@@ -0,0 +1,14 @@
+import gradio as gr
+from fastai.vision.all import *
+import skimage
+
+learn = load_learner('export.pkl')
+
+def classify_image(img):
+    img = PILImage.create(img)
+    pred, pred_idx, probs = learn.predict(img)
+    return {labels[i]: float(probs[i]) for i in range(len(labels))}
+
+title = 'Greek God Classifier'
+description = 'A greek god classifier trained on data from the internet. It classifies images (often inaccurately) of the six main gods'
+gr.Interface(fn=predict, inputs=gr.inputs.Image(shape=(512, 512)), outputs=gr.outputs.Label(num_top_classes=3), title = title, description = description).launch(share=True)
